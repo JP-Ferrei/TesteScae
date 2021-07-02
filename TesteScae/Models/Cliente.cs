db6c6c8e1e27c7cd8d0b9cadacd3e966a6eaae78
Joao Pedro <jpfdc9@gmail.com>
2021-07-02 19:02:54 -0300

    Add project files.

---
+++ TesteScae/Models/Cliente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteScae.Models {
+    public class Cliente {
+
+
+        [Key]
+        public int Id { get; private set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Cpf { get; set; }
+
+        [ForeignKey("EnderecoId")]
+        public int EnderecoId { get; private set; }
+
+               
+
+        public Cliente(int id, string nome, string cpf) {
+            Id = id;
+            Nome = nome;
+            Cpf = cpf;
+           
+        }
+    }
+}
---
+++ TesteScae/Models/Endereco.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteScae.Models {
+    public class Endereco {
+
+        [Key]
+        public int Id { get; private set; }
+        
+        public string Rua { get; set; }
+        
+        public string Bairro { get; set; }
+        
+        public string Cidade { get; set; }
+        
+        public string Estado { get; set; }
+
+
+        public Endereco(string rua, string bairro, string cidade, string estado){
+            this.Rua = rua;
+            this.Bairro =bairro;
+            this.Cidade =cidade;
+            this.Estado = estado;
+        }
+    }
+}
